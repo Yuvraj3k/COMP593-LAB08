811fa18e5215a04bed69cc0a95bff73acbd0d62f
Yuvraj3k <Yuvraj3k@users.noreply.github.com>
2024-07-19 16:29:26 -0400

    update

--- marriage_report.py
+++ marriage_report.py
@@ -5,10 +5,13 @@ Description:
 
 Usage:
  python marriage_report.py
-"""
+""" 
+
 import os
 import sqlite3
+import pandas as pd
 from create_relationships import db_path, script_dir
+
 
 def main():
     # Query DB for list of married couples
@@ -18,16 +21,31 @@ def main():
     csv_path = os.path.join(script_dir, 'married_couples.csv')
     save_married_couples_csv(married_couples, csv_path)
 
+
 def get_married_couples():
     """Queries the Social Network database for all married couples.
 
     Returns:
         list: (name1, name2, start_date) of married couples 
     """
-    # TODO: Function body
+    #TODOFunction body
     # Hint: See example code in lab instructions entitled "Get a List of Relationships"
     con = sqlite3.connect(db_path)
-    return
+    cur = con.cursor()
+    
+    married_couples_query = """
+    SELECT person1.name, person2.name, start_date FROM relationships
+    JOIN people AS person1 ON person1_id = person1.id
+    JOIN people AS person2 ON person2_id = person2.id
+    WHERE type = 'spouse';
+    """
+    
+    cur.execute(married_couples_query)
+    married_couples = cur.fetchall()
+    
+    con.close()
+    return married_couples
+
 
 def save_married_couples_csv(married_couples, csv_path):
     """Saves list of married couples to a CSV file, including both people's 
@@ -37,9 +55,12 @@ def save_married_couples_csv(married_couples, csv_path):
         married_couples (list): (name1, name2, start_date) of married couples
         csv_path (str): Path of CSV file
     """
-    # TODO: Function body
+    # TODOFunction body
     # Hint: We did this in Lab 7.
+    df = pd.DataFrame(married_couples, columns=['Person 1', 'Person 2', 'Anniversary'])
+    df.to_csv(csv_path, index=False)
     return
+
 
 if __name__ == '__main__':
    main()
